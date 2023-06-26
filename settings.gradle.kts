d4302e2b0375c6ce46c1d2c2fe3ae5fb49b994cb
GitHub Action <action@github.com>
2023-06-26 15:14:36 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "clips-language-highlighting-intellij"
--- src/main/kotlin/com/github/dritanium/clipslanguagehighlightingintellij/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/dritanium/clipslanguagehighlightingintellij/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.dritanium.clipslanguagehighlightingintellij.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.dritanium.clipslanguagehighlightingintellij.MyBundle
+import com.github.dritanium.clipslanguagehighlightingintellij.services.MyProjectService
 import javax.swing.JButton
 
 
